2a6fbe1d76aa9f985c7dfb4e2de28fec6d47dc9c
Pratham Dogra <dogra.pratham@gmail.com>
2019-10-23 22:26:53 +0530

    Added model

---
+++ models/employee.model.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const employeeSchema = new Schema({
+  S_NO :{
+    required: false,
+    type: Number
+  },
+  CASE_STATUS :{
+    required: true,
+    type: String
+  },
+  EMPLOYER_NAME :{
+    required: true,
+    type: String
+  },
+  SOC_NAME :{
+    required: true,
+    type: String
+  },
+  JOB_TITLE :{
+    required: true,
+    type: String
+  },
+  FULL_TIME_POSITION :{
+    required: true,
+    type: String
+  },
+  PREVAILING_WAGE :{
+    required: true,
+    type: String
+  },
+  YEAR :{
+    required: true,
+    type: String
+  },
+  WORKSITE :{
+    required: true,
+    type: String
+  },
+  lon :{
+    required: true,
+    type: String
+  },
+  Last :{
+      required: true,
+      type: String
+  }
+});
+
+module.exports = mongoose.model('Employee', employeeSchema);
